90adda685c10b7a3ed5c8d8f2d0eb885b2975697
Paul van Coller <paul.vancoller@adeptadvisory.co.za>
2020-03-24 22:40:15 +0200

    Created flyway migration to change existing client configurations to require consent.

--- db/sql/ids4/V1__require_consent_for_existing_clients.sql
+++ db/sql/ids4/V1__require_consent_for_existing_clients.sql
@@ -1,2 +1,9 @@
+--
+-- *************************************************
+-- Copyright (c) 2020, Grindrod Bank Limited
+-- License MIT: https://opensource.org/licenses/MIT
+-- **************************************************
+--
+
 -- This script updates any current client configurations to now require consent.
 update _ids4."Clients" set "RequireConsent" = true;
