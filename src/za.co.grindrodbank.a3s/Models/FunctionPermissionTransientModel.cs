0a3b7471b8c25c9d40c7f8cc08d297ee8183e0ef
Victor Szewczenko <szewczenkov@gmail.com>
2020-04-09 09:34:25 +0200

    Adding missing license headers to new files.

--- src/za.co.grindrodbank.a3s/Models/FunctionPermissionTransientModel.cs
+++ src/za.co.grindrodbank.a3s/Models/FunctionPermissionTransientModel.cs
@@ -1,3 +1,9 @@
+/**
+ * *************************************************
+ * Copyright (c) 2020, Grindrod Bank Limited
+ * License MIT: https://opensource.org/licenses/MIT
+ * **************************************************
+ */
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
