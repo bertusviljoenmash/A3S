dd436c962e7114ab1094def03dfa9915d3e68934
Victor Szewczenko <szewczenkov@gmail.com>
2020-04-09 09:27:23 +0200

    Adding the most fundamental function transient models.

---
+++ src/za.co.grindrodbank.a3s/Models/FunctionPermissionTransientModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace za.co.grindrodbank.a3s.Models
+{
+    public class FunctionPermissionTransientModel 
+    {
+        [Required]
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public Guid FunctionId { get; set; }
+        [Required]
+        public Guid PermissionId { get; set; }
+    }
+}
---
+++ src/za.co.grindrodbank.a3s/Models/FunctionTransientModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace za.co.grindrodbank.a3s.Models
+{
+    [Table("FunctionTransient")]
+    public class FunctionTransient : TransientStateMachineRecord
+    {
+        [Required]
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public Guid RoleId { get; set; }
+
+        [Required]
+        public Guid SubRealmId { get; set; }
+
+        [NotMapped]
+        public List<FunctionPermissionTransientModel> LatestTransientRolePermissions { get; set; }
+    }
+}
